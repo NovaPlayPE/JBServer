04b8f390623e6b3c809d0bcde13b65ec98d736d0
Ragnok123 <gebab.urumci.kv@gmail.com>
2021-05-21 13:02:30 +0200

    fix style...

--- src/main/java/net/novatech/jbserver/JBMain.java
+++ src/main/java/net/novatech/jbserver/JBMain.java
@@ -4,37 +4,37 @@ import net.novatech.jbserver.server.Server;
 import net.novatech.library.utils.SystemOS;
 
 public class JBMain {
-	
+
 	public final static String API_VERSION = "1.0.0";
 	public final static String VERSION = "1.0";
 	public final static String PATH = System.getProperty("user.dir") + "/";
-    public final static String DATA_PATH = System.getProperty("user.dir") + "/";
-    public final static String PLUGIN_PATH = DATA_PATH + "plugins";
-    public final static String WORLD_PATH = DATA_PATH + "worlds";
-    public static final long START_TIME = System.currentTimeMillis();
-    public static boolean ANSI = true;
-    public static boolean shortTitle = false;
-    public static int DEBUG = 1;
-	
+	public final static String DATA_PATH = System.getProperty("user.dir") + "/";
+	public final static String PLUGIN_PATH = DATA_PATH + "plugins";
+	public final static String WORLD_PATH = DATA_PATH + "worlds";
+	public static final long START_TIME = System.currentTimeMillis();
+	public static boolean ANSI = true;
+	public static boolean shortTitle = false;
+	public static int DEBUG = 1;
+
 	public static void main(String[] args) {
-		if(SystemOS.CURRENT_OS == SystemOS.WINDOWS) {
+		if (SystemOS.CURRENT_OS == SystemOS.WINDOWS) {
 			shortTitle = true;
 		}
-        for (String arg : args) {
-            switch (arg) {
-                case "disable-ansi":
-                    ANSI = false;
-                    break;
-            }
-        }
+		for (String arg : args) {
+			switch (arg) {
+			case "disable-ansi":
+				ANSI = false;
+				break;
+			}
+		}
 
-        try {
-            if (ANSI) {
-                System.out.print((char) 0x1b + "]0;Starting JBServer for network listening" + (char) 0x07);
-            }
-            Server server = new Server(PATH, DATA_PATH, PLUGIN_PATH, WORLD_PATH);
-        } catch (Exception e) {
-        }
+		try {
+			if (ANSI) {
+				System.out.print((char) 0x1b + "]0;Starting JBServer for network listening" + (char) 0x07);
+			}
+			Server server = new Server(PATH, DATA_PATH, PLUGIN_PATH, WORLD_PATH);
+		} catch (Exception e) {
+		}
 	}
 
 }
