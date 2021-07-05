c838655b6d26fc0d7c3ea35fd6802dfa26458437
Ragnok123 <gebab.urumci.kv@gmail.com>
2021-07-05 21:04:44 +0200

    Update JBBedrockSession.java

--- src/main/java/net/novatech/jbserver/network/bedrock/JBBedrockSession.java
+++ src/main/java/net/novatech/jbserver/network/bedrock/JBBedrockSession.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import net.novatech.jbprotocol.bedrock.BedrockSession;
 import net.novatech.jbprotocol.bedrock.packets.BedrockPacket;
+import net.novatech.jbprotocol.bedrock.packets.MovePlayerPacket;
 import net.novatech.jbprotocol.packet.AbstractPacket;
 import net.novatech.jbserver.event.player.PlayerPacketReceiveEvent;
 import net.novatech.jbserver.event.player.PlayerPacketSendEvent;
@@ -12,6 +13,7 @@ import net.novatech.jbserver.network.bedrock.retranslator.BedrockRetranslatorSec
 import net.novatech.jbserver.network.protocol.JBPacket;
 import net.novatech.jbserver.player.Player;
 import net.novatech.jbserver.player.bedrock.BedrockPlayer;
+import net.novatech.library.math.Vector3d;
 
 public class JBBedrockSession implements NetworkSession {
 	
@@ -45,6 +47,23 @@ public class JBBedrockSession implements NetworkSession {
 		
 		PlayerPacketReceiveEvent event = new PlayerPacketReceiveEvent(getPlayer(), packet);
 		event.call();
+		
+		if(packet instanceof MovePlayerPacket) {
+			MovePlayerPacket pk = (MovePlayerPacket)packet;
+			Vector3d pos = pk.position.asDouble().clone();
+			
+			if(pos.distanceSquared(getPlayer().getLocation().getVector()) < 0.01 
+					&& pk.rotation.getYaw() % 360 == getPlayer().getLocation().getYaw()
+					&& pk.rotation.getPitch() % 360 == getPlayer().getLocation().getPitch()) {
+				return;
+			}
+			if(pos.distanceSquared(getPlayer().getLocation().getVector()) > 100) {
+				//sendPosition
+				return;
+			}
+			boolean revert = false;
+			
+		}
 	}
 
 }
