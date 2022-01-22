0bf63838c7266723c8b7bba60e0f8d33e6344ed0
Ragnok123 <gebab.urumci.kv@gmail.com>
2022-01-22 18:10:48 +0100

    hm

--- src/main/java/net/novatech/jbserver/network/protocol/ProtocolInfo.java
+++ src/main/java/net/novatech/jbserver/network/protocol/ProtocolInfo.java
@@ -2,9 +2,9 @@ package net.novatech.jbserver.network.protocol;
 
 public class ProtocolInfo {
 	
-	public static final int BEDROCK_PROTOCOL = 440;
-	public static final String BEDROCK_VERSION = "1.17.0";
-	public static final int JAVA_PROTOCOL = 0x4000001B;
-	public static final String JAVA_VERSION = "1.17.0";
+	public static final int BEDROCK_PROTOCOL = 475;
+	public static final String BEDROCK_VERSION = "1.18.2";
+	public static final int JAVA_PROTOCOL = 575;
+	public static final String JAVA_VERSION = "1.18.1";
 	
 }
